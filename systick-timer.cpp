63f3d90f09599d1d8f415b781cf47333bfe1f8b6
Pavel Burgr <pb@drake.cz>
2020-01-18 00:53:15 +0100

    WIP

--- systick-timer.cpp
+++ systick-timer.cpp
@@ -5,9 +5,9 @@ namespace genericTimer {
 	void initHardware() {
 		target::SYSTICK.RVR.setRELOAD(clkHz / 100L);
 		target::SYSTICK.CVR.setCURRENT(0);
-		target::SYSTICK.CSR.setTICKINT(1);
-		target::SYSTICK.CSR.setCLKSOURCE(1);
-		target::SYSTICK.CSR.setENABLE(1);
+		target::SYSTICK.CSR.setTICKINT(target::systick::CSR::TICKINT::DISABLE_SYSTICK_EXCEPTION);
+		target::SYSTICK.CSR.setCLKSOURCE(target::systick::CSR::CLKSOURCE::CPU_CLOCK);
+		target::SYSTICK.CSR.setENABLE(target::systick::CSR::ENABLE::ENABLED);
 	}
 
 }
